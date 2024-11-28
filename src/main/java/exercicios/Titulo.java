171a3de66c04be1b1e73ac7a764b7c0a8f1cd84f
Marina Gajego <marinagajego1377@gmail.com>
2024-11-27 22:54:34 -0300

    Criando a estrutura correta projeto java

---
+++ src/main/java/exercicios/Titulo.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+public class Titulo implements Comparable<Titulo>{
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int compareTo(Titulo outro) {
+        return this.nome.compareTo(outro.nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+}
